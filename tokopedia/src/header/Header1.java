1ffd433839cf191787717a0b07e4c291ef9cb6d4
Yan Wibisono <yan.wibisono@gmail.com>
2025-03-01 16:22:15 +0700

    commit added click tentang tokopedia link

--- tokopedia/src/header/Header1.java
+++ tokopedia/src/header/Header1.java
@@ -1,16 +1,23 @@
 package header;
 
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Header1 {
 
+	@SuppressWarnings("deprecation")
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
 		System.setProperty("webdriver.chrome.driver", "C:\\BrowserDriver\\chromedriver.exe");
 		ChromeDriver driver = new ChromeDriver();
 		driver.get("https://www.tokopedia.com");
-		// edited from eclipse by yan wibisono
+		driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
+		
+		//Open Tentang Tokopedia
+		driver.findElement(By.xpath("//a[@class='css-1wx1rev'][normalize-space()='Tentang Tokopedia']")).click();
 	}
 
 }
